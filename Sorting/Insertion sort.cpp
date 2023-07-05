26d3ce296e63c9fd2dd280368de0cc97dc52639b
Devanshi Kapla <89089904+devanshikapla@users.noreply.github.com>
2023-07-05 17:23:00 +0530

    Create Insertion sort.cpp

---
+++ Sorting/Insertion sort.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> 
+void insertionSort(int n, vector<int> &arr){
+    // Write your code here.
+    for(int i = 0 ; i < n ; i++){
+        int j = i;
+        while(j > 0 && arr[j - 1] > arr[j]){
+            swap(arr[j] , arr[j - 1]);
+            j--;
+        }
+    }
+}
